9922ae777e2068dcfd7b273031fd570054a3d7b2
Osaca13 <odlyss@gmail.com>
2018-01-10 23:28:43 +0100

    Agregar archivos de proyecto.

---
+++ TerraeJobSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TerraeJobSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
